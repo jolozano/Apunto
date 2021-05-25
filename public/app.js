883b5d42e8a92809640a91bd02e50f7db46d0005
Jose E. Lozano Jr <josee.lozanojr.@MacBook-Pro.local>
2021-05-25 09:13:58 -0500

    added top 5 results from GET to page

--- public/app.js
+++ public/app.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
-    console.log('linked')
+    console.log('You can do this!!');
+
+    const $searchText = $('#searchBox');
+    const $searchButton = $('#searchButton');
+    const $subjectBox = $('.subjectBox');
+    const $postText = $('.postText');
+    const $postButton =  $('.post');
+    const $resultsContainer = $('.results')
+
+    function getText($var) {
+        let result = $var.val();
+        return result;
+    }
+    $.get('http://localhost:3000/api/posts', (data) => {
+            for (let index of data) {
+                console.log(typeof index.date)
+                let $divHeader = $(`<div class='header'>Date: ${index.date.slice(0,16)} Subject: ${index.subject}</div>`)
+                let $divPost = $(`<div class='resultPost'>${index.posts}</div>`);
+                $divHeader.appendTo($resultsContainer);
+                $divPost.appendTo($resultsContainer);
+            }
+    })
+
+    $searchButton.on('click', () => {
+        let result = getText($searchText);
+        console.log(result);
+        return result;
+    });
+    $postButton.on('click', () => {
+        let result = {subject: $subjectBox.val(), posts: $postText.val()};
+        console.log(result);
+        return result;
+    })
 })
--- seed.sql
+++ seed.sql
@@ -1,4 +1,4 @@
--- INSERT INTO posts (date, subject, posts) VALUES ('2021-05-24', 'initial', 'it worked');
+-- INSERT INTO posts (date, subject, posts) VALUES (CURRENT_TIMESTAMP, 'initial', 'it worked');
 -- INSERT INTO posts (date, subject, posts) VALUES ('2021-05-24', 'database', 'I found out how to insert current date into the column of my database.');
 -- INSERT INTO posts (date, subject, posts) VALUES ('2021-05-24', 'database', 'I reseeded my db a couple of times because of dumb mistakes.');
 -- INSERT INTO posts (date, subject, posts) VALUES ('2021-05-24', 'database', 'I updated my seed file.');
@@ -14,3 +14,7 @@
 -- INSERT INTO posts (date, subject, posts) VALUES (2021-05-24, 'database', 'well it displays properly in the psql but not in the data.rows. oh well moving on to the next step. bigger fish to fry.');
 -- INSERT INTO posts (date, subject, posts) VALUES (2021-05-24, 'project', 'Well, I think I am done for the day. I think I am at a good stopping point. I have the barebones of my server and index.html. I was able to send a GET request with the psql. I will work on the app.js tomorrow morning. I need a break.');
 -- INSERT INTO posts (date, subject, posts) VALUES (CURRENT_TIMESTAMP, 'overtime', 'working a little longer on the project before bed. kinda excited about what I can do. I am sure that will subside when I encounter real problems but really excited to prove what I am capable of.');
+-- INSERT INTO posts (date, subject, posts) VALUES (CURRENT_TIMESTAMP, 'New 2', 'its the begining of day two of my MVP project. Didnt sleep too much. My youngest was sick. Thats why there is coffee and I have a lot of it. :-)');
+-- INSERT INTO posts (date, subject, posts) VALUES (CURRENT_TIMESTAMP, 'database', 'I think I might have to change the data type of the posts column to varchar.....I am not sure if that will help ......');
+-- INSERT INTO posts (date, subject, posts) VALUES (CURRENT_TIMESTAMP, 'front end', 'well i just got the get request to work on the front end. I know have to display it. Things are making sense right now. feeling good.');
+-- INSERT INTO posts (date, subject, posts) VALUES (CURRENT_TIMESTAMP, 'GET', 'displayed top 5 results on page, lets hope the good work keeps coming');
