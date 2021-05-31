e3b9711b91566c7edcc6f6f437b6be7340065acf
Jose E. Lozano Jr <josee.lozanojr.@MacBook-Pro.local>
2021-05-31 16:59:44 -0500

    wip

--- server.js
+++ server.js
@@ -52,6 +52,17 @@ app.post('/api/posts', (req, res) => {
     });
 })
 
+app.delete('/api/posts', (req, res) => {
+    pool.query("DELETE FROM posts WHERE id=$1;", [req.query.id], (err, data) => {
+        if (err) {
+            res.status(404).send('NOT FOUND')
+        } else {
+            console.log(data.rows)
+            res.json("DELETED POST");
+        }
+    })
+})
+
 app.listen(process.env.PORT, function() {
     console.log(`Server is Running and FMC on ${process.env.PORT}`);
 });
