3574051b344f0d0b394ebeb77b69306252bab2d9
Jose Lozano <jose.e.lozano.jr@gmail.com>
2021-07-27 16:51:13 -0500

    altered function

--- public/app.js
+++ public/app.js
@@ -13,14 +13,15 @@ $(document).ready(function() {
     gapi.load('auth2', function() {
         gapi.auth2.init();
     })
-    $ssoButton.on('click', (googleUser) => {
+    const getInfo = (googleUser) => {
         let profile = googleUser.getBasicProfile();
         console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
         console.log('Name: ' + profile.getName());
         console.log('Image URL: ' + profile.getImageUrl());
         console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    })
-    
+    }
+    $ssoButton.on('click', getInfo())
+
     const deletePost = (id) => {
         $.ajax({
             url: '/api/posts' + '?' + $.param(id),
