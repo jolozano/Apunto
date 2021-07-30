81f05162af21079a0a5328d58a592787e14fdf43
Jose Lozano <jose.e.lozano.jr@gmail.com>
2021-07-29 19:27:09 -0500

    moved app.js script from html down to the very bottom of head

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
 
+$(document).ready(function() {
     const $searchText = $('#searchBox');
     const $searchButton = $('#searchButton');
     const $subjectBox = $('#subjectBox');
