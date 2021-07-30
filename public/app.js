7d2016779f16543aa3694e061eedd96a696e04fe
Jose Lozano <jose.e.lozano.jr@gmail.com>
2021-07-29 19:36:33 -0500

    moved signin function back to index.html

--- public/app.js
+++ public/app.js
@@ -11,23 +11,6 @@ $(document).ready(function() {
     let $del;
     let verified = null;
 
-    function onSignIn(googleUser) {
-        var profile = googleUser.getBasicProfile();
-        var id_token = googleUser.getAuthResponse().id_token;
-        console.log('ID Token: ' + id_token)
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', 'https://intense-thicket-66788.herokuapp.com/api/tokensignin');
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.onload = function() {
-        console.log('Signed in as: ' + xhr.responseText);
-        };
-        xhr.send('idtoken=' + id_token);
-    }
-
     function signOut() {
         var auth2 = gapi.auth2.getAuthInstance();
         auth2.signOut().then(function () {
@@ -35,7 +18,6 @@ $(document).ready(function() {
         });
     }
     $ssoSignOut.on('click', signOut());
-    $ssoSignIn.on('click', onSignIn());
 
     if (!verified) {
         $resultsContainer.hide();
