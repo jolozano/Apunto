73a8b291cc946fa8442476282e3f924f1fd439bf
Jose Lozano <jose.e.lozano.jr@gmail.com>
2021-07-30 13:54:35 -0500

    using sign out button to display content to test concept with hide

--- public/app.js
+++ public/app.js
@@ -14,18 +14,13 @@ $(document).ready(function() {
     const clearContent = () => {
         $bodyContainer.hide()
         $resultsContainer.hide();
-        $searchContainer.hide();
+        $searchContainer.empty();
     }
     function signOut() {
         var auth2 = gapi.auth2.getAuthInstance();
         auth2.signOut().then(function () {
         console.log('User signed out.');
-        verified = !verified;
-        if (!verified) {
-        clearContent();
-        } else {
-            console.log('authorized')
-        }
+        console.log(auth2.signOut())
         });
     }
 
