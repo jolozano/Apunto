feae1ffaaa24b240cc1a5fd0eeba3af341fb2301
Jose Lozano <jose.e.lozano.jr@gmail.com>
2021-07-30 13:38:59 -0500

    using sign out button to display content to test concept

--- public/app.js
+++ public/app.js
@@ -9,12 +9,15 @@ $(document).ready(function() {
     const $SSOsignOut = $('#signOut')
     const $bodyContainer = $('.bodyContainer')
     let $del;
-    let verified = null;
+    let verified = false;
 
     function signOut() {
         var auth2 = gapi.auth2.getAuthInstance();
         auth2.signOut().then(function () {
         console.log('User signed out.');
+        console.log(verified)
+        verified = !verified;
+        console.log(verified)
         });
     }
 
