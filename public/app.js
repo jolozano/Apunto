cf74210aa92e21abd755a381b8c60b50d9d45c37
Jose Lozano <jose.e.lozano.jr@gmail.com>
2021-07-30 14:44:39 -0500

    testing html script

--- public/app.js
+++ public/app.js
@@ -25,67 +25,60 @@ $(document).ready(function() {
 
     $SSOsignOut.on('click', signOut)
 
+    const deletePost = (id) => {
+        $.ajax({
+            url: '/api/posts' + '?' + $.param(id),
+            type: 'DELETE',
+            success: (res) => {
+                console.log(res);
+            }
+        })
+    }
 
-    if (!verified) {
-        clearContent()
-    } else {
-        const deletePost = (id) => {
-            $.ajax({
-                url: '/api/posts' + '?' + $.param(id),
-                type: 'DELETE',
-                success: (res) => {
-                    console.log(res);
-                }
-            })
+    $.get('/api/posts', (data) => {
+        for (let index of data) {
+            let $divHeader = $(`<div class='header'>Date: ${index.date.slice(0,16)}---->Subject: ${index.subject}
+            ---->${index.post}<form><button id=${index.id} class='del'>Delete</button></form></div>`);
+            $divHeader.appendTo($resultsContainer);
         }
-    
-        $.get('/api/posts', (data) => {
+        $del = $('.del');
+            $del.on('click', (e) => {
+                let idParam = {id: e.target.id};
+                console.log(idParam)
+                deletePost(idParam);
+            })
+    })
+
+    $searchButton.on('click', (event) => {
+        event.preventDefault();
+        let searchText = {search: $searchText.val()};
+        $.get('/api/posts', searchText, (data) => {
+            $resultsContainer.hide();
+            $searchContainer.empty();
             for (let index of data) {
                 let $divHeader = $(`<div class='header'>Date: ${index.date.slice(0,16)}---->Subject: ${index.subject}
                 ---->${index.post}<form><button id=${index.id} class='del'>Delete</button></form></div>`);
-                $divHeader.appendTo($resultsContainer);
+                $divHeader.appendTo($searchContainer);
             }
             $del = $('.del');
-                $del.on('click', (e) => {
-                    let idParam = {id: e.target.id};
-                    console.log(idParam)
-                    deletePost(idParam);
-                })
-        })
-    
-        $searchButton.on('click', (event) => {
-            event.preventDefault();
-            let searchText = {search: $searchText.val()};
-            $.get('/api/posts', searchText, (data) => {
-                $resultsContainer.hide();
-                $searchContainer.empty();
-                for (let index of data) {
-                    let $divHeader = $(`<div class='header'>Date: ${index.date.slice(0,16)}---->Subject: ${index.subject}
-                    ---->${index.post}<form><button id=${index.id} class='del'>Delete</button></form></div>`);
-                    $divHeader.appendTo($searchContainer);
-                }
-                $del = $('.del');
-                $del.on('click', (e) => {
-                    let idParam = {id: e.target.id};
-                    console.log(idParam)
-                    deletePost(idParam);
-                })
+            $del.on('click', (e) => {
+                let idParam = {id: e.target.id};
+                console.log(idParam)
+                deletePost(idParam);
             })
-        });
-    
-        $postButton.on('click', () => {
-            let post = {subject: $subjectBox.val(), post: $postText.val()};
-            $.post(
-                {
-                    url: '/api/posts',
-                    data: JSON.stringify(post),
-                    success: (res) => {
-                        console.log(res);
-                },
-                contentType: "application/json"
-            });
-        });
+        })
+    });
 
-    }
-
+    $postButton.on('click', () => {
+        let post = {subject: $subjectBox.val(), post: $postText.val()};
+        $.post(
+            {
+                url: '/api/posts',
+                data: JSON.stringify(post),
+                success: (res) => {
+                    console.log(res);
+            },
+            contentType: "application/json"
+        });
+    });
 })
